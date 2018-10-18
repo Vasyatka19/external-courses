accb5ff9e6dff270da9476dbe13db5cf3a6066f8
vasyatka19 <epam@local.rsu.edu.ru>
2018-10-18 21:09:20 +0300

    task01

---
+++ src/ex5_js-functions/task01.js
@@ -0,0 +1,9 @@
+let calculator = {
+    add: function add() {},
+    subtract: function subtract() {},
+    divide: function divide() {},
+    multiply: function multiply() {},
+    getResult: function getResult() {},
+    reset: function reset() {},
+
+}
