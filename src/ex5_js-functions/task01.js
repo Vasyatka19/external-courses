eb7398389fa49b8ca162cd8f3661c415663d097a
Vasyatka19 <andr__@list.ru>
2018-10-19 17:13:06 +0300

    Калькулятор

--- src/ex5_js-functions/task01.js
+++ src/ex5_js-functions/task01.js
@@ -1,9 +1,51 @@
 let calculator = {
-    add: function add() {},
-    subtract: function subtract() {},
-    divide: function divide() {},
-    multiply: function multiply() {},
-    getResult: function getResult() {},
-    reset: function reset() {},
-
+    add:  (a) => {
+        if(typeof a === 'number'){
+            calculator.result += a;
+        }
+        return  (b) => {return calculator.add(b);}
+    },
+    subtract:  (a) => {
+        if(typeof a === 'number'){
+            calculator.result -= a;
+        }
+        return  (b) => {return calculator.subtract(b);}
+    },
+    divide: (a) => {
+        if(typeof a === 'number'){
+            calculator.result /= a;
+        }
+        return  (b) => {return calculator.divide(b);}
+    },
+    multiply: (a) => {
+        if(typeof a === 'number'){
+            calculator.result *= a;
+        }
+        return  (b) => {return calculator.multiply(b);}
+    },
+    getResult: () => {
+        return calculator.result;
+    },
+    reset: () => {
+        calculator.result = 0;
+    },
+    result: 0
 }
+
+calculator.add(5)(2)()(8)(12)();
+console.log(calculator.getResult());
+
+calculator.reset();
+console.log(calculator.getResult());
+
+calculator.add(5)(2)()(8)(12)();
+console.log(calculator.getResult());
+
+calculator.subtract(5)(2)()(8)(2)();
+console.log(calculator.getResult());
+
+calculator.divide(2)(2)();
+console.log(calculator.getResult());
+
+calculator.multiply(4)(2)();
+console.log(calculator.getResult());
