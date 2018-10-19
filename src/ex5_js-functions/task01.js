84c6b04048ff93a5c66f5a89698c436b5118d492
vasyatka19 <epam@local.rsu.edu.ru>
2018-10-19 21:01:19 +0300

    set change

--- src/ex5_js-functions/task01.js
+++ src/ex5_js-functions/task01.js
@@ -1,36 +1,40 @@
-let calculator = {
-    add:  function(a) {
-        if(typeof a === 'number'){
-            calculator.getResult(calculator.getResult()+a);
+function calc(){
+    let result = 0;
+    return {
+        add:  function(a)  {
+            if(typeof a === 'number'){
+                result += a;
+            }
+            return  function(b)  {return calculator.add(b);}
+        },
+        subtract:  function(a) {
+            if(typeof a === 'number'){
+                result -= a;
+            }
+            return  function(b) {return calculator.subtract(b);}
+        },
+        divide: function(a)  {
+            if(typeof a === 'number'){
+                result /= a;
+            }
+            return  function(b) {return calculator.divide(b);}
+        },
+        multiply: function(a) {
+            if(typeof a === 'number'){
+                result *= a;
+            }
+            return  function(b) {return calculator.multiply(b);}
+        },
+        getResult: function() {
+            return result;
+        },
+        reset: function() {
+            result = 0;
         }
-        return function(b) {return calculator.add(b);}
-    },
-    // subtract:  function(a) {
-    //     if(typeof a === 'number'){
-    //         result -= a;
-    //     }
-    //     return  function(b) {return calculator.subtract(b);}
-    // },
-    // divide: function(a) {
-    //     if(typeof a === 'number'){
-    //         result /= a;
-    //     }
-    //     return  function(b) {return calculator.divide(b);}
-    // },
-    // multiply: function(a) {
-    //     if(typeof a === 'number'){
-    //         result *= a;
-    //     }
-    //     return  function(b) {return calculator.multiply(b);}
-    // },
-    // getResult: function(a) {
-    //     var result = a;
-    //     return function() { return result};
-    // },
-    reset: function() {
-        calculator.result = 0;
     }
 }
+
+let calculator = calc();
 
 calculator.add(5)(2)()(8)(12)();
 console.log(calculator.getResult());
