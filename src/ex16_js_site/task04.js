0720c191b1598ca3dd215a0f5f83ff9c8aaaf1d9
vasyatka19 <andr__@list.ru>
2018-12-06 00:13:24 +0300

    task16

--- src/ex16_js_site/task04.js
+++ src/ex16_js_site/task04.js
@@ -18,8 +18,8 @@ Book.prototype.getElement = function(){
 
 
 function BookScreen(){
-    this.allBooksList = [new Book("Jevels of Nizam","Geeta Davi",5,"sources/book-cover1.jpg",155,5),
-                    new Book('Cakes and Bakes','Sanjeev Kapoor',5,'sources/book-cover2.jpg',1255,10),
+    this.allBooksList = [new Book("Jevels of Nizam","Geeta Davi",4,"sources/book-cover1.jpg",155,5),
+                    new Book('Cakes and Bakes','Sanjeev Kapoor',2,'sources/book-cover2.jpg',1255,10),
                     new Book('Cakes and Bakes','Sanjeev Kapoor',5,'sources/book-cover3.jpg',155,5),
                     new Book('Cakes and Bakes','Sanjeev Kapoor',5,'sources/book-cover4.jpg',155,0),
                     new Book('Cakes and Bakes','Sanjeev Kapoor',5,'sources/book-cover5.jpg',0,5),
@@ -37,25 +37,21 @@ BookScreen.prototype.getBookList = function(){
     return this.bookList;
 }
 BookScreen.prototype.filterBookList = function(paramName,criterial){
-    switch (paramName){
-        case bookName: { this.bookList = this.bookList.filter(function(el){
-            el.bookName.includes(criterial);
-        })}
-        case bookRating: {this.bookList = this.bookList.filter(function(el){
-            el.bookRating > criterial;
-        })}
-        case bookBuyCount: {this.bookList = this.bookList.filter(function(el){
-            el.bookBuyCount > criterial;
-        })}
-        case bookPrice: {this.bookList = this.bookList.filter(function(el){
-            el.bookPrice = criterial;
-        })}
-        default: {return this.bookList.reduce();}
+    this.bookList = this.allBooksList.filter(function(el){
+        switch(paramName){
+            case "bookName": {return el.bookName.toLowerCase().includes(criterial.toLowerCase());}
+            case "bookRating": {return el.bookRating >= 4;}
+            case "bookBuyCount": {return el.bookBuyCount >= 4;}
+            case "bookPrice": {return el.bookPrice === 0;}
+            default: {return true;}
+        }
     }
-    
+    )
 }
+
 BookScreen.prototype.bookShow = function(){
     let filmTag = document.getElementById("films__list");
+    filmTag.innerHTML = '';
     for(let i =0;i<this.bookList.length;i++){
         filmTag.appendChild(this.bookList[i].getElement());
     } 
@@ -65,5 +61,29 @@ BookScreen.prototype.bookShow = function(){
 const BOOKSCREEN = new BookScreen();  
 
 window.onload = function() {
-    BOOKSCREEN.bookShow();
+     BOOKSCREEN.bookShow();
+
+    let filters = document.getElementsByClassName('films__menu--item');
+    for(let i = 0; i<filters.length; i++){
+        filters[i].addEventListener('click',addFilter.bind(null,filters[i]));
+    }
+    let search = document.forms['bookName'];
+    search.addEventListener('click',addSearch.bind(null,search));
  };
+
+ function addFilter(el){
+    let filters = document.getElementsByClassName('films__menu--item');
+    for(let i = 0; i<filters.length; i++){
+        filters[i].style.backgroundColor = '#edf0f6';
+    }
+     el.style.backgroundColor = "#96b2cd";
+     BOOKSCREEN.filterBookList(el.getAttribute('name'));
+     BOOKSCREEN.bookShow();
+ }
+ function addSearch(el){
+    let criterial = el.elements["search"].value;
+    if(criterial!==null){
+        BOOKSCREEN.filterBookList('bookName',criterial);
+        BOOKSCREEN.bookShow();
+    }
+ }
