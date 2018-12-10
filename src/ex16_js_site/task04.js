91f14f0ac60c587e7cf9a4c5ea83c3bc19c889db
Vasyatka19 <andr__@list.ru>
2018-12-10 18:05:26 +0300

    create request

--- src/ex16_js_site/task04.js
+++ src/ex16_js_site/task04.js
@@ -1,24 +1,24 @@
-function Book(bookName,bookAuthor,bookRating,bookImage,bookPrice,bookBuyCount){
-    this.bookName = bookName;
-    this.bookAuthor = bookAuthor;
-    this.bookRating = bookRating;
-    this.bookImage = bookImage;
+function Book(title,author,rating,image_url,cost,bookBuyCount){
+    this.title = title;
+    this.author = author;
+    this.rating = rating;
+    this.image_url = image_url;
     this.bookBuyCount = bookBuyCount;
-    this.bookPrice = bookPrice;
-    this.bookID = Book.count + 1;
+    this.cost = cost;
+    this.id = Book.count + 1;
     Book.count += 1;
 }
 Book.count = 0;
 Book.prototype.setRating = function(rating){
-    this.bookRating = rating;
+    this.rating = rating;
 }
 Book.prototype.getElement = function(){
     var div = document.createElement('div');
     div.setAttribute("class","films__list--item")
-    div.innerHTML = '<img src="' + this.bookImage + '" alt="1">\
-        <div>' + this.bookName + '</div>\
-        <div>' + this.bookAuthor + '</div>\
-        <div class="films__list--star">' + getStars(this.bookRating,this.bookID) + '</div>'
+    div.innerHTML = '<img src="' + this.image_url + '" alt="1">\
+        <div>' + this.title + '</div>\
+        <div>' + this.author + '</div>\
+        <div class="films__list--star">' + getStars(this.rating,this.id) + '</div>'
     return div;
 
     function getStars(count,id){
@@ -35,30 +35,25 @@ Book.prototype.getElement = function(){
 }
 
 
-function BookScreen(){
-    this.allBooksList = [new Book("Jevels of Nizam","Geeta Davi",4,"sources/book-cover1.jpg",155,5),
-                    new Book('Cakes and Bakes','Sanjeev Kapoor',2,'sources/book-cover2.jpg',1255,10),
-                    new Book('Cakes and Bakes','Sanjeev Kapoor',5,'sources/book-cover3.jpg',155,5),
-                    new Book('Cakes and Bakes','Sanjeev Kapoor',5,'sources/book-cover4.jpg',155,0),
-                    new Book('Cakes and Bakes','Sanjeev Kapoor',5,'sources/book-cover5.jpg',0,5),
-                    new Book('Cakes and Bakes','Sanjeev Kapoor',5,'sources/book-cover6.jpg',1855,25),
-                    new Book('Cakes and Bakes','Sanjeev Kapoor',5,'sources/book-cover7.jpg',155,5),
-                    new Book('Cakes and Bakes','Sanjeev Kapoor',5,'sources/book-cover8.jpg',0,5),
-                    new Book('Cakes and Bakes','Sanjeev Kapoor',5,'sources/book-cover9.jpg',155,15),
-                    new Book('Cakes and Bakes','Sanjeev Kapoor',5,'sources/book-cover10.jpg',0,40)];
+function BookScreen(allBooksList){
+    this.allBooksList = allBooksList;  
+    for (let i = 0; i <this.allBooksList.length; i++){
+        this.allBooksList[i].__proto__ = Book.prototype;
+    }
     this.bookList = this.allBooksList;
+    console.log (this.bookList);
 }
-BookScreen.prototype.addBook = function(bookName,bookAuthor,bookRating,bookImage,bookPrice,bookBuyCount){
-    this.allBooksList.push(new Book(bookName,bookAuthor,bookRating,bookImage,bookPrice,bookBuyCount));
+BookScreen.prototype.addBook = function(title,author,rating,image_url,cost,bookBuyCount){
+    this.allBooksList.push(new Book(title,author,rating,image_url,cost,bookBuyCount));
     this.bookList = this.allBooksList;
 }
 BookScreen.prototype.filterBookList = function(paramName,criterial){
     this.bookList = this.allBooksList.filter(function(el){
         switch(paramName){
-            case "bookName": {return el.bookName.toLowerCase().includes(criterial.toLowerCase());}
-            case "bookRating": {return el.bookRating >= 4;}
+            case "bookName": {return el.title.toLowerCase().includes(criterial.toLowerCase());}
+            case "bookRating": {return el.rating >= 4;}
             case "bookBuyCount": {return el.bookBuyCount >= 4;}
-            case "bookPrice": {return el.bookPrice === 0;}
+            case "bookPrice": {return el.cost === 0;}
             default: {return true;}
         }
     }
@@ -73,57 +68,61 @@ BookScreen.prototype.bookShow = function(){
     } 
 }
 
-
-const BOOKSCREEN = new BookScreen();  
-
-window.onload = function() {
-    BOOKSCREEN.bookShow();
-  
-    let filmsList = document.getElementById('films__list');
-    filmsList.onclick = function(event){
-        var target = event.target;
-        while (target != filmsList) {
-            if (target.parentNode.getAttribute('class') == 'films__list--star') {
-            addBookRating(target);
-            return;
-            }
-            target = target.parentNode;
-        }
-    }   
-
-    let filters = document.getElementById('films__menu');
-    filters.onclick = function(event){
-        var target = event.target;
-        while (target != this) {
-            if (target.getAttribute('class') == 'films__menu--item') {
-            addFilter(target);
-            return;
-            }
-            target = target.parentNode;
-        }
-    }
-
-    let search = document.forms['bookName'];
-    search.addEventListener('click',addSearch.bind(null,search));
-
-    let addBookButton = document.getElementsByClassName('menu__button--add')[0];
-    addBookButton.addEventListener('click',addBookFormVisible);
-
-    let addBookForm = document.forms['book__add'];
-    let add = addBookForm.elements["add"];
-    add.addEventListener('click',addBookInList.bind(null,addBookForm));
-
-    addBookForm.onclick = function(event){
-        let target = event.target;
-        while(target != this){
-            if(target.getAttribute('class') == "close"){
-                close(target.parentNode);
+makeRequest("https://rsu-library-api.herokuapp.com/books");
+let BOOKSCREEN;
+function createBookList(response){
+    BOOKSCREEN = new BookScreen(response);  
+   // window.onload = function() {
+        BOOKSCREEN.bookShow();
+    
+        let filmsList = document.getElementById('films__list');
+        filmsList.onclick = function(event){
+            var target = event.target;
+            while (target != filmsList) {
+                if (target.parentNode.getAttribute('class') == 'films__list--star') {
+                addBookRating(target);
                 return;
+                }
+                target = target.parentNode;
             }
-            target = target.parentNode;
+        }   
+
+        let filters = document.getElementById('films__menu');
+        filters.onclick = function(event){
+            var target = event.target;
+            while (target != this) {
+                if (target.getAttribute('class') == 'films__menu--item') {
+                addFilter(target);
+                return;
+                }
+                target = target.parentNode;
+            }
         }
-    } 
- }
+
+        let search = document.forms['bookName'];
+        search.addEventListener('click',addSearch.bind(null,search));
+
+        let addBookButton = document.getElementsByClassName('menu__button--add')[0];
+        addBookButton.addEventListener('click',addBookFormVisible);
+
+        let addBookForm = document.forms['book__add'];
+        let add = addBookForm.elements["add"];
+        //add.addEventListener('click',makeRequest.bind(null,"https://rsu-library-api.herokuapp.com/books"));
+        add.addEventListener('click',addBookInList.bind(null,addBookForm));
+        //makeRequest
+
+        addBookForm.onclick = function(event){
+            let target = event.target;
+            while(target != this){
+                if(target.getAttribute('class') == "close"){
+                    close(target.parentNode);
+                    return;
+                }
+                target = target.parentNode;
+            }
+        } 
+  //  }
+}
 
  function addFilter(el){
     let filters = document.getElementsByClassName('films__menu--item');
@@ -177,11 +176,11 @@ window.onload = function() {
      BOOKSCREEN.bookShow();
  }
  function addBookRating(el){
-     let id = el.getAttribute('id');
-     let bookID = parseInt(id.substring(0,id.indexOf("__")));
-     let rating = parseInt(id.substring(id.indexOf("__")+2,id.length));
+     let bookId = el.getAttribute('id');
+     let ID = parseInt(bookId.substring(0,bookId.indexOf("__")));
+     let rating = parseInt(bookId.substring(bookId.indexOf("__")+2,bookId.length));
      BOOKSCREEN.allBooksList.filter(function(elem){
-         return elem.bookID === bookID;
+         return elem.id === ID;
      })[0].setRating(rating)
      BOOKSCREEN.bookShow();
  }
@@ -189,3 +188,32 @@ window.onload = function() {
  function close(el){
      el.style.display = 'none'
  }
+
+
+ function makeRequest(url) {
+	var httpRequest;
+
+	if (window.XMLHttpRequest) {
+        httpRequest = new XMLHttpRequest();
+        httpRequest.responseType = 'json';
+	} 
+
+	if (!httpRequest) {
+		alert('Giving up :( Cannot create an XMLHTTP instance');
+		return false;
+    }
+	httpRequest.onreadystatechange = function() { loadContents(httpRequest); };
+	httpRequest.open('GET', url, true);
+    httpRequest.send('');
+    //return httpRequest.response;
+}
+
+function loadContents(httpRequest) {
+	if (httpRequest.readyState == 4) {
+		if (httpRequest.status == 200) {
+            createBookList(httpRequest.response);
+		} else {
+			alert('There was a problem with the request.');
+		}
+	}
+}
